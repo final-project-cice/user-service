261e3c45a6d0e9994598db035a45e9ee78f51ace
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-07-08 16:27:17 +0200

    Adding tests to UserServiceImpl.

--- src/test/java/com/trl/users/service/impl/UserServiceImplTest.java
+++ src/test/java/com/trl/users/service/impl/UserServiceImplTest.java
@@ -1,16 +1,63 @@
 package com.trl.users.service.impl;
 
+import com.trl.users.controller.dto.UserDTO;
+import com.trl.users.exceptions.ExceptionUserWithEmailExist;
+import com.trl.users.repository.UserRepository;
+import com.trl.users.repository.entity.UserEntity;
+import com.trl.users.service.UserService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserServiceImplTest {
 
-    // TODO: Utilizar los Mocks.
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private UserService userService;
+
+    private UserEntity userEntity;
+    private UserDTO userDTO;
+    private UserDTO userDTOExpected;
 
     @Before
     public void setUp() throws Exception {
-        // TODO: Terminar este test.
+
+        userEntity = new UserEntity()
+                .setId(1L)
+                .setFirstName("User_FirstName_1")
+                .setLastName("User_LastName_1")
+                .setEmail("user_1@email.com")
+                .setPassword("strong_password")
+                .setBirthday(new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime());
+
+        userDTO = new UserDTO()
+                .setFirstName("User_FirstName_1")
+                .setLastName("User_LastName_1")
+                .setEmail("user_1@email.com")
+                .setPassword("strong_password")
+                .setBirthday(new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime());
+
+        userDTOExpected = new UserDTO()
+                .setFirstName("User_FirstName_144444444444444444444")
+                .setLastName("User_LastName_1")
+                .setEmail("user_1@email.com")
+                .setPassword("strong_password")
+                .setBirthday(new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime());
     }
 
     @After
@@ -19,8 +66,29 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void create() {
+    public void create() throws ExceptionUserWithEmailExist {
         // TODO: Terminar este test.
+//        System.out.println(userRepository.findById(1L));
+
+//        UserDTO userCreated = userService.create(userDTO);
+
+//        Mockito.doReturn(userEntity).when(userRepository).save(new UserEntity()
+//                .setId(1L)
+//                .setFirstName("User_FirstName_1")
+//                .setLastName("User_LastName_1")
+//                .setEmail("user_1@email.com")
+//                .setPassword("strong_password")
+//                .setBirthday(new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime()));
+
+//        System.out.println("*************************************************" + userCreated);
+
+
+//        UserDTO userDTOActual = Mockito.verify(userService, Mockito.times(1)).create(userDTO);
+//        Assert.assertEquals(userDTOExpected, userDTOActual);
+//        UserEntity save = Mockito.verify(userRepository, Mockito.times(1)).save(userEntity);
+
+
+        UserDTO userDTO = Mockito.doReturn(userDTOExpected).when(userService).create(this.userDTO);
     }
 
     @Test
