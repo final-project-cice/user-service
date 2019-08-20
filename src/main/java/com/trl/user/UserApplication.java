dbcb9db60255d6aebdef0f9fed11c61b7ec9861d
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-08-20 10:28:01 +0200

    Refactoring.

--- src/main/java/com/trl/user/UserApplication.java
+++ src/main/java/com/trl/user/UserApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MsShopUsersApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsShopUsersApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
