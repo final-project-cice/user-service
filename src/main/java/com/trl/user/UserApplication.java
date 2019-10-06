d70054a8d010f430239d26122f28a53d296bb12d
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-10-06 21:33:47 +0200

    Refactoring. Removing annotations unnecessary.

--- src/main/java/com/trl/user/UserApplication.java
+++ src/main/java/com/trl/user/UserApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class UserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
-
 }
